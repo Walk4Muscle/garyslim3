2afe55f3f0f9b195aa85b8f48af7e7b1e3611c76
Walk4Muscle <304839635@qq.com>
2016-03-09 23:20:54 +0800

    03/09

--- models/roleModel.class.php
+++ models/roleModel.class.php
@@ -2,6 +2,9 @@
 require_once '/utility/medooHelper.class.php';
 
 class roleModel extends medooHelper {
+	/**
+	 *	view role - role_access - access table
+	 **/
 	public function listView() {
 		$db = $this->getDB();
 		$result = $db->select("role", [
@@ -16,7 +19,31 @@ class roleModel extends medooHelper {
 			"accesses.action(action)",
 			"accesses.alias(accesses_alias)",
 		]);
-		var_dump($db->error());
+		// var_dump($db->error());
+		return $this->returnData($result);
+	}
+
+	/**
+	 * @Author   Gary Liu
+	 * @DateTime 2016-03-09T23:18:37+0800
+	 * @param    [integer] $role_id
+	 * @param    [array(integer)|array(object)] $access_ids 
+	 * @return   [array]
+	 */
+	public function access($role_id,$access_ids){
+		$db = $this->getDB();
+		$insert_data = [];
+		foreach ($access_ids as $key => $value) {
+			if(is_integer($value)){
+				array_push($insert_data,['role_id'=>$role_id,'access_id'=>$value]);
+			}
+			if(isset($value['access_id'])){
+				array_push($insert_data,['role_id'=>$role_id,'access_id'=>$value['access_id']]);
+			}else{
+				$error = ['status'=>false,'error'=>'Invalid data'];
+			}
+		}
+		$result = $db->select("role_accesses",$insert_data);
 		return $this->returnData($result);
 	}
 }
